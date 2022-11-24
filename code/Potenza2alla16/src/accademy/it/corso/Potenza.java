1ae8b82e47c9eef23edbab12c6da3a8a99b54f69
michelebellizzi <bellizzi23@gmail.com>
2022-11-24 17:35:29 +0100

    PotenzaMethod

--- code/Potenza2alla16/src/accademy/it/corso/Potenza.java
+++ code/Potenza2alla16/src/accademy/it/corso/Potenza.java
@@ -3,14 +3,30 @@ package accademy.it.corso;
 import java.util.Scanner;
 
 public class Potenza {
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		CalcoloPotenza calcoloPotenza= new CalcoloPotenza();
-		int valoreFinale= calcoloPotenza.calcola();
-	     
-	     System.out.println(valoreFinale);
-	}
-	
-}
+		Scanner scanner= new Scanner(System.in);
+		System.out.println("inserisci numero: ");
+		 int numero = scanner.nextInt();
+		 System.out.println("inserisci elevazione: ");
+	        int potenza = scanner.nextInt();
+		System.out.println(calcola(numero, potenza));
+		
 
+	}
+	public static int calcola(int numero, int potenza) {
+		Scanner scanner= new Scanner (System.in);
+		
+	        int risultato = 1;
+	        int i = potenza;
+	        
+	        for (;i != 0; i--)
+	        {
+	            risultato *= numero;
+	            
+	           
+	        }
+	       return risultato;
+	}
+}
