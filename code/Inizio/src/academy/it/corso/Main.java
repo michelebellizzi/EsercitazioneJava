8f7c01fe9ed8ec5ab93d0f09717b3b4dfec64f4e
michelebellizzi <bellizzi23@gmail.com>
2022-11-24 12:35:31 +0100

    nomeCognomeJava

--- code/Inizio/src/academy/it/corso/Main.java
+++ code/Inizio/src/academy/it/corso/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello/n");
+		System.out.println("hello \n");
 		System.out.println("Michele bellizzi");
 	}
 
