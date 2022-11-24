da8b6730c2edff7b46a6c428d7192bb6c85ab918
michelebellizzi <bellizzi23@gmail.com>
2022-11-24 17:48:53 +0100

    Ricorsione

---
+++ code/Ricorsione/src/accademy/it/corso/Ricorsione.java
@@ -0,0 +1,27 @@
+package accademy.it.corso;
+
+public class Ricorsione {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=fattoriale(130);
+		System.out.println(num);
+		
+		
+
+	}
+
+	public static int fattoriale(int n) {
+		
+		
+		if(n%2 != 0) {
+			return 0;
+	
+		}
+		else{
+return 1+fattoriale(n/2);
+		}
+
+
+	}
+}
